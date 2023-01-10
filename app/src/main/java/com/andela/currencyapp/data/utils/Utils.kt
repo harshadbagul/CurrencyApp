ab00a2292ebfb99af3aa297f6451f1afdb63cba9
harshad <harshadbagul444@gmail.com>
2023-01-10 13:39:03 +0530

    Added changes for connectivity

--- app/src/main/java/com/andela/currencyapp/data/utils/Utils.kt
+++ app/src/main/java/com/andela/currencyapp/data/utils/Utils.kt
@@ -1,8 +1,12 @@
 package com.andela.currencyapp.data.utils
 
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Build
-import androidx.annotation.RequiresApi
-import java.util.*
+import androidx.appcompat.app.AlertDialog
+import com.andela.currencyapp.R
 import kotlin.collections.ArrayList
 
 object Utils {
@@ -15,4 +19,36 @@ object Utils {
         }
         return popularCurrencies
     }
+
+
+    fun Context.showErrorDialog(
+                        title:String?= null,
+                        message: String? = this.getString(R.string.message_network_alert_dialog)){
+        val alertDialog: AlertDialog = AlertDialog.Builder(this).create()
+        alertDialog.setTitle(title ?: this.getString(R.string.default_error_text))
+        alertDialog.setMessage(message)
+        alertDialog.setButton(
+            AlertDialog.BUTTON_POSITIVE, this.getString(R.string.button_okay)
+        ) { dialog, _ -> dialog.dismiss() }
+        alertDialog.show()
+    }
+
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectionManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectionManager.getNetworkCapabilities(connectionManager.activeNetwork)
+            if (networkCapabilities == null) {
+                false
+            } else {
+                networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                        networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) &&
+                        networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED)
+            }
+        } else {
+            // below Marshmallow version
+            val activeNetwork = connectionManager.activeNetworkInfo
+            activeNetwork?.isConnectedOrConnecting == true && activeNetwork.isAvailable
+        }
+    }
+
 }
